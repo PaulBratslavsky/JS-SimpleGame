9220c2f2fc8c0951b64ce1548c976197ac7ff7d6
Paul <paul.bratslavsky@gmail.com>
2019-09-18 13:13:57 -0500

    Finished

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ var currentPlayer = document.querySelector(`#current-${activePlayer}`);
 var newGameButton = document.querySelector('.btn-new');
 
 document.querySelector('.dice').style.display = 'none';
+document.querySelector(`.player-${activePlayer}-panel`).classList.add('active');
+
+
 
 /*******************************
     EVENT LISTENERS
@@ -33,8 +36,13 @@ function newGame() {
   document.getElementById('score-0').textContent = 0;
   document.getElementById('score-1').textContent = 0;
   document.querySelector('.dice').style.display = 'none';
+  document.querySelector('#name-0').textContent = 'Player 1'; 
+  document.querySelector('#name-1').textContent = 'Player 2';   
   activePlayer = 0;
   playersScore = [0,0];
+  rollDiceButton.style.display = 'block';
+  holdTurnButton.style.display = 'block';
+
   console.log('New game started');
 } 
 /*********************************************************/
@@ -64,12 +72,15 @@ function handleDiceRoll() {
   if (currentRoll == 1) {
     document.querySelector(`#current-${activePlayer}`).textContent = 0;
     activePlayer ? activePlayer = 0 : activePlayer = 1;
+
+    showActivePlayer();
   }
 }
 
 /*********************************************************/
 function handleHoldTurn() {
   console.log('Hold turn button clicked!');
+
   
   // 1. Get Temp Score
   var tempPlayerScore = document.querySelector(`#current-${activePlayer}`);
@@ -78,8 +89,13 @@ function handleHoldTurn() {
   var newScore = parseInt(tempPlayerScore.textContent) + playersScore[activePlayer];
   playersScore[activePlayer] = newScore;
 
-  console.log(playersScore[activePlayer], 'sup');
-  console.log(playersScore);
+  console.log(playersScore[activePlayer], 'befor you won');
+
+  if ( playersScore[activePlayer] >= 21 ) {
+    youDidIt();
+  }
+
+  console.log('after youWon()');
 
   // 3. Show Running Total
   showRunningTotalPlayerScore();
@@ -90,6 +106,8 @@ function handleHoldTurn() {
   } else {
     activePlayer = 1;
   }
+
+  showActivePlayer();
 
   // 5. Reset Temp Score to 0
   tempPlayerScore.textContent = 0;
@@ -115,6 +133,48 @@ function showRunningTotalPlayerScore() {
   console.log(currentPlayerScoreTotal, "running total");
 
 }
+
+/*********************************************************/
+function youDidIt() {
+  console.log(activePlayer, "won");
+
+  var winner;
+  var loser;
+
+  if ( activePlayer === 1 ) {
+      winner = 1;
+      loser = 0
+  } else {
+      winner = 0;
+      loser = 1
+  }
+  
+  document.querySelector(`#name-${winner}`).textContent = 'Winner'; 
+  document.querySelector(`#name-${loser}`).textContent = 'Loser'; 
+
+  rollDiceButton.style.display = 'none';
+  holdTurnButton.style.display = 'none';
+}
+
+/*********************************************************/
+function showActivePlayer() {
+  console.log('test');
+  var show;
+  var hide;
+
+  if ( activePlayer === 1 ) {
+    show = 1;
+    hide = 0;
+  } else {
+    show = 0;
+    hide = 1
+  } 
+
+  document.querySelector(`.player-${show}-panel`).classList.add('active');
+  document.querySelector(`.player-${hide}-panel`).classList.remove('active');
+}
+
+/*********************************************************/
 
 
 
